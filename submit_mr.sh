b311265ac4f7a5ea367963861dae57e84e1d1559
Junyang Qian <qianjunyang@gmail.com>
2019-11-27 10:23:28 -0800

    Modify the submission file to accommodate plink2.0

--- submit_mr.sh
+++ submit_mr.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 #
 module load R/3.4.0
+module load plink2
+module load zstd
 today=`date +%m%d%y`
 dirLog="$mr/log/"
 [ -d "$dirLog" ] || mkdir -p "$dirLog"
@@ -10,10 +12,11 @@ dirLog="$mr/log/"
 
 # log files written to $dirLog/mr_${today}_${rank[$idx]}_${weighted[$idx]}.err, $dirLog/mr_${today}_${rank[$idx]}_${weighted[$idx]}.out
 
-rank=(8 16 39)
+rank=(8 16 35)
 prev=(0 0 0) # which iteration to start with; -1 means starting from the last intermediate results
-mmem=(120000 120000 250000)
+mmem=(250000 250000 250000)
 weighted=(0 0 0)
+use_plink2=(1 1 1) # whether to use plink2 for matrix multiplication, 1 for 2.0, 0 for 1.9.
 
 for idx in "${!rank[@]}"; do
   sbatch -p stat,mrivas --mail-type=END,FAIL --mail-user=junyangq@stanford.edu \
@@ -22,5 +25,5 @@ for idx in "${!rank[@]}"; do
        --output=$dirLog/mr_${today}_${rank[$idx]}_${weighted[$idx]}.out\
        --error=$dirLog/mr_${today}_${rank[$idx]}_${weighted[$idx]}.err \
        --job-name="mr_${rank[$idx]}_${weighted[$idx]}" \
-       --wrap="Rscript ./job_mr.R ${rank[$idx]} ${prev[$idx]} ${weighted[$idx]}"
+       --wrap="Rscript ./job_mr.R ${rank[$idx]} ${prev[$idx]} ${weighted[$idx]} ${use_plink2[$idx]}"
 done
